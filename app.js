6287315fd7343453ecd9901192442b4b4d8e60c5
Joey <joeygeof.dev@gmail.com>
2023-03-07 16:17:52 -0500

    Update: url parse function using POST, with text form and submit button

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // exits event loop for the server:
-    // process.exit();
-    //
-    // send a html response with the request
+    // parse the url
+    const url = req.url;
+    // add condition that executes only when the url is both a string and has a specific value
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Submit Message</title><head>');
+        // add a form with text inputs and a submit button
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>');
+        res.write('<html>');
+        // add return to exit from this set of functions
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title><head>');
