baebaf21397d34a6330954d4b9447a69494b135d
Joey <joeygeof.dev@gmail.com>
2023-03-07 16:43:39 -0500

    Update: method parse function with message and POST condition then redirect

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const filesys = require('fs');
 
 const server = http.createServer((req, res) => {
     // parse the url
     const url = req.url;
+    // parse the method
+    const method = req.method;
     // add condition that executes only when the url is both a string and has a specific value
     if (url === '/') {
         res.write('<html>');
@@ -11,6 +14,15 @@ const server = http.createServer((req, res) => {
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>');
         res.write('<html>');
         // add return to exit from this set of functions
+        return res.end();
+    }
+    // add new condition to parse the url 'message' but only if it is a POST request
+    if (url === '/message' && method === 'POST') {
+        // create a new file and store the message
+        filesys.writeFileSync('form.txt', 'Dummy text');
+        // add return to exit from function and redirect back to /
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
