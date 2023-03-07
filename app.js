6fe8d2b5af40c2750612de2b447a3736f526ebb2
Joey <joeygeof.dev@gmail.com>
2023-03-06 22:24:22 -0500

    Update: exit event loop

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req);
+    // exits event loop for the server:
+    // process.exit();
 });
 
 server.listen(5000);
